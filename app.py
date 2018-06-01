67017b159118a560dcf1a34f6dff198780e0fffc
Vincent Chiang <vincentchiang@berkeley.edu>
2018-06-01 15:59:51 -0700

    set up database with sqlalchemy

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from flask_mysqldb import MySQL
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@Database123@localhost/hospitalityapp'
-db = SQLAlchemy(app)
+
+engine = create_engine('sqlite:///webmgmt.db', convert_unicode=True, echo=False)
 
 app.debug = True
 
 @app.route('/')
 def home():
+    db = SQLAlchemy(app)
+    db.init_app(app)
+    db.create_all()
     return render_template('home.html')
 
+class Hospitals(db.Model):
+    __table__ = db.Model.metadata.tables['patientdata']
+    def __repr__(self):
+        return self.drg
 
 # at the bottom to run the app
 if __name__ == '__main__':
+    # from sqlalchemy.orm import scoped_session, sessionmaker, Query
+    # db_session = scoped_session(sessionmaker(bind=engine))
+    # for item in db_session(Hospitals.zipcode, Hospitals.city):
+    #     print (item)
     app.run()
--- models.py
+++ models.py
@@ -1,1 +1,12 @@
-from models import User
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@Database123@localhost/hospitalityapp'
+db = SQLAlchemy(app)
+db.Model.metadata.reflect(db.engine)
+
+class Hospitals(db.Model):
+    __table__ = db.Model.metadata.tables['patientdata']
+    def __repr__(self):
+        return self.name 
